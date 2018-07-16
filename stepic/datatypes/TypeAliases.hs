d38bc9ffd186622988c9afa4ba370b6830408bb2
mikla <mikla.op@gmail.com>
2018-07-16 11:37:55 +0300

    Monoids.

--- stepic/datatypes/TypeAliases.hs
+++ stepic/datatypes/TypeAliases.hs
@@ -31,3 +31,29 @@ newtype Product a = Product {getProduct :: a}
 instance Num a => Monoid (Product a) where
   mempty = Product 1
   Product x `mappend` Product y = Product(x * y)
+
+newtype Xor = Xor {getXor :: Bool} deriving (Eq, Show)
+
+instance Monoid Xor where
+  mempty = Xor False
+  Xor x `mappend` Xor y = Xor (x /= y)
+
+instance (Monoid a, Monoid b) => Monoid (a, b) where
+  mempty = (mempty, mempty)
+  (x1, y1) `mappend` (x2, y2) =
+    (x1 `mappend` x2, y1 `mappend` y2)
+
+
+newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
+    deriving (Eq,Show)
+
+--instance Monoid a => Monoid (Maybe' a) where
+--  mempty = Maybe' $ Just mempty
+--  mappend (Maybe' Nothing) _ = Maybe' Nothing
+--  mappend _ (Maybe' Nothing) = Maybe' Nothing
+--  mappend (Maybe' a) (Maybe' b) = Maybe' (mappend a b)
+
+newtype Endo a = Endo {getEndo :: a -> a}
+instance Monoid (Endo a) where
+  mempty = Endo id
+  Endo a `mappend` Endo b = Endo (a . b)
