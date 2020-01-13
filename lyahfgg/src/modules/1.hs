e4ac31fbe482c40dbb74c7591938ce8c105ba70a
Artsiom Miklushou <mikla.op@gmail.com>
2020-01-13 11:04:23 +0200

    Modules.

--- lyahfgg/src/modules/1.hs
+++ lyahfgg/src/modules/1.hs
@@ -1,7 +1,9 @@
-module Module where
+module Module(exportFunctionFromModule) where
 
 import Data.List
 import Data.List(filter)
 import Data.List hiding (map)
 import qualified Data.List as L
 
+exportFunctionFromModule :: String -> String
+exportFunctionFromModule x = x
